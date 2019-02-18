8710f78a3c61466586e5522838d6c4ff65491273
Pierson Brannan <plbrannan95@gmail.com>
2019-02-18 11:40:07 -0500

    Init of file, npm installs, component boilerplate from planning

---
+++ src/Components/DeleteQuestion/DeleteQuestion.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DeleteQuestion extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default DeleteQuestion;
