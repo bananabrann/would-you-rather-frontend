db325bc50d84377ae1b75511b02772f91b677b5a
Pierson Brannan <plbrannan95@gmail.com>
2019-02-18 16:06:09 -0500

    Paths working

--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,10 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route, Link, Redirect } from "react-router-dom";
 
 class Homepage extends Component {
     render() {
         return (
             <div>
                 <p>Hello from Homepage!</p>
+                <Link to="/play">
+                    <button>Play!</button>
+                </Link>
+                <Link to="/stats">
+                    <button>Stats</button>
+                </Link>
+                <Link to="/options">
+                    <button>Options</button>
+                </Link>
+
+                <Route
+                    path="/path"
+                    exact
+                    render={routerProps => <Redirect to="/play" />}
+                />
+                <Route
+                    path="/stats"
+                    exact
+                    render={routerProps => <Redirect to="/stats" />}
+                />
+                <Route
+                    path="/options"
+                    exact
+                    render={routerProps => <Redirect to="/options" />}
+                />
             </div>
         );
     }
