1815ebd315ded3aae088ff87dbeec2f61b0b0521
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-19 17:37:00 -0500

    the user can now add new questions to the DB

--- src/Components/NewQuestion/NewQuestion.js
+++ src/Components/NewQuestion/NewQuestion.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class NewQuestion extends Component {
+    constructor(){
+        super()
+
+        this.handleChange = this.handleChange.bind(this)
+        this.submit=this.submit.bind(this)
+    }
+
+    submit(e) {
+        e.preventDefault()
+        console.log(e.target.value)
+        axios.post('http://localhost:3001/create', this.state)
+          .catch((err) => {
+            console.log(err)
+          })
+    }
+    handleChange(evt) {
+        const variable = evt.target.name
+        this.setState({[variable]: evt.target.value })
+    }
+
     render() {
         return (
             <div className="container">
                 <h1 className="addTitle" >Would You Rather:</h1>
-                <form action="/create" method="post" className="addNewQuestions">
-                    <textarea rows='4' cols= '50' name="question1" className="question1" /><br />
+                <form  onSubmit= {this.submit} className="addNewQuestions">
+                    <textarea rows='4' cols= '50' name="question1" className="question1" onChange={this.handleChange} /><br />
                     <h1> OR </h1>
-                    <textarea rows='4' cols= '50' name="question2" className="question2" /><br />
-                    <input type="submit" value="Submit" />
+                    <textarea rows='4' cols= '50' name="question2" className="question2" onChange={this.handleChange} /><br />
+                    <input type="submit" value="poop" name="submit"/>
                 </form>
 
             </div>
