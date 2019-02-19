e1eb5809e9d49405963b92623938842c7ec6aaa3
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-19 16:47:38 -0500

    edit NewQuestion

--- src/Components/NewQuestion/NewQuestion.js
+++ src/Components/NewQuestion/NewQuestion.js
@@ -5,10 +5,10 @@ class NewQuestion extends Component {
         return (
             <div className="container">
                 <h1 className="addTitle" >Would You Rather:</h1>
-                <form method="post" className="addNewQuestions">
-                    <textarea rows='4' cols= '50' className="question[question1]" /><br />
+                <form action="/create" method="post" className="addNewQuestions">
+                    <textarea rows='4' cols= '50' name="question1" className="question1" /><br />
                     <h1> OR </h1>
-                    <textarea rows='4' cols= '50' className="question[question2]" /><br />
+                    <textarea rows='4' cols= '50' name="question2" className="question2" /><br />
                     <input type="submit" value="Submit" />
                 </form>
 
