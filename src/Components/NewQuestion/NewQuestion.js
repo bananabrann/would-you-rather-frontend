2483080aa2ff98a49100be68d019f51293701687
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-19 16:11:38 -0500

    added input fields for NewQuestion

--- src/Components/NewQuestion/NewQuestion.js
+++ src/Components/NewQuestion/NewQuestion.js
@@ -4,7 +4,14 @@ class NewQuestion extends Component {
     render() {
         return (
             <div className="container">
-                <p>Hello from NewQuestion!</p>
+                <h1 className="addTitle" >Would You Rather:</h1>
+                <form method="post" className="addNewQuestions">
+                    <textarea rows='4' cols= '50' className="question[question1]" /><br />
+                    <h1> OR </h1>
+                    <textarea rows='4' cols= '50' className="question[question2]" /><br />
+                    <input type="submit" value="Submit" />
+                </form>
+
             </div>
         );
     }
