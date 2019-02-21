7fa6edd58e92f437be8c013957018c006a418e54
Rendon Elsesser <rendonelsesser@gmail.com>
2019-02-20 21:36:52 -0500

    refactored a bit for DRY, removed extra non-essential

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,6 +10,7 @@ import NewQuestion from "../NewQuestion/NewQuestion";
 import EditQuestion from "../EditQuestion/EditQuestion";
 import Options from "../Options/Options";
 import { Route } from "react-router-dom";
+
 const url = "http://localhost:3001";
 
 class App extends Component {
@@ -19,28 +20,13 @@ class App extends Component {
             questionData: []
         };
         this.deleteQuestion = this.deleteQuestion.bind(this);
-        this.destroy = this.destroy.bind(this);
     }
+
     componentDidMount() {
         console.log("Play: Component is mounting");
         axios.get(url + "/play").then(json => {
             this.setState({ questionData: json.data });
         });
-    }
-
-    // need to take the current state, with all the questions
-    // iterate through and remove the current selected question, return and array
-
-    destroy(evt) {
-        evt.preventDefault();
-        console.log("hi");
-        axios
-            .delete("http://localhost:3001/edit", {
-                data: { id: evt.id }
-            })
-            .catch(err => {
-                console.log(err);
-            });
     }
 
     deleteQuestion(evt) {
@@ -51,41 +37,13 @@ class App extends Component {
             questionData: questionData.filter(q => q._id !== questionId)
         });
         axios
-            .delete("http://localhost:3001/edit", {
+            .delete(url + "/edit", {
                 data: { id: questionId }
             })
             .catch(err => {
                 console.log(err);
             });
     }
-
-    deleteQuestion(evt) {
-        evt.preventDefault();
-        const questionBank = this.state.questionData
-        // const { questionData } = this.state
-        const i = questionBank.indexOf(evt)
-        const updatedBank = questionBank.slice( 0, i).concat(questionBank.slice(i + 1, questionBank.length))
-        // questionBank = without(tempApts, apt);
-        
-        this.setState({
-            questionData: updatedBank
-        });
-        axios
-
-    // deleteQuestion(evt) {
-    //     evt.preventDefault();
-
-    //     this.setState({
-    //         questionData: this.state.questionData.filter((q) => q.id !== evt.id)
-    //       })
-    //     axios
-    //         .delete("http://localhost:3001/edit", {
-    //             data: {id: evt.id }
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //         });
-    //   }
 
     render() {
         return (
@@ -95,7 +53,8 @@ class App extends Component {
                 <Route
                     exact
                     path="/play"
-                    render={() => <Play questions={this.state.questionData} />}
+                    render={() => <Play 
+                                    questions={this.state.questionData} />}
                 />
                 <Route exact path="/stats" render={() => <Stats />} />
                 <Route exact path="/options" render={() => <Options />} />
@@ -105,9 +64,8 @@ class App extends Component {
                     path="/edit"
                     render={() => (
                         <EditQuestion
-                            destroy={this.state.destroy}
-                            delete={this.deleteQuestion}
                             questions={this.state.questionData}
+                            delete={this.deleteQuestion}
                         />
                     )}
                 />
--- src/Components/EditQuestion/EditQuestion.js
+++ src/Components/EditQuestion/EditQuestion.js
@@ -3,30 +3,16 @@ import React, { Component } from "react";
 
 class EditQuestion extends Component {
 
-    // destroy(evt) {
-    //     evt.preventDefault();
-
-    //     axios
-    //         .delete("http://localhost:3001/edit", {
-    //             data: {id: evt.id }
-    //         })
-    //         .catch(err => {
-    //             console.log(err);
-    //         });
-    // }
 
     render() {
-        console.log("EditQuestion: rendered");
         const question = this.props.questions.map((question, i) => {
             return (
                 <div>
-                    {/* <h1>{question._id}</h1> */}
                     <form id={question._id} onSubmit={this.props.delete}>
 
                         <h4 key={question._id}>{question.question1} OR {question.question2}</h4>
 
                         <button >Delete</button>
-                        {/* onClick={() => this.destroy(question)} */}
                         <button>edit </button>
 
                     </form>
